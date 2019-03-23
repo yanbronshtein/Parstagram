0c79da5600e3703484fa220ce2ad1de270e7073e
Yaniv Bronshtein <yanbronshtein@gmail.com>
2019-03-23 11:34:23 -0400

    Buggy

---
+++ Parstagram/LoginViewController.swift
@@ -0,0 +1,51 @@
+//
+//  LoginViewController.swift
+//  Parstagram
+//
+//  Created by Yaniv Bronshtein on 3/21/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var userNameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        
+    }
+    @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = userNameField.text
+        user.password = passwordField.text
+        user.signUpInBackground {
+            (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
